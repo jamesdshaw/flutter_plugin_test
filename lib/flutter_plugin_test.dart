6b0e4d42caa7fce3d1f2bec19b58b23ad9b39939
James Shaw <thejamesshaw@gmail.com>
2024-01-28 13:10:26 -0500

    feat: updating message

--- lib/flutter_plugin_test.dart
+++ lib/flutter_plugin_test.dart
@@ -2,7 +2,8 @@
 import 'flutter_plugin_test_platform_interface.dart';
 
 class FlutterPluginTest {
-  Future<String?> getPlatformVersion() {
-    return FlutterPluginTestPlatform.instance.getPlatformVersion();
+  Future<String?> getPlatformVersion() async {
+    final platform = await FlutterPluginTestPlatform.instance.getPlatformVersion();
+    return 'Platform: $platform + oh hi there';
   }
 }
